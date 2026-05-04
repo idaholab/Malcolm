9f6ce4cc0adb8a1be2f56827fb1d44bc8bec44d5
Seth Grover <seth.d.grover@gmail.com>
2026-05-04 15:34:01 +0000

    Fluent bit to v5.0.4

--- scripts/third-party-logs/fluent-bit-setup.ps1
+++ scripts/third-party-logs/fluent-bit-setup.ps1
@@ -9,7 +9,7 @@
 ###############################################################################
 
 $fluent_bit_version = '5.0'
-$fluent_bit_full_version = '5.0.2'
+$fluent_bit_full_version = '5.0.4'
 
 ###############################################################################
 # select an item from a menu provided in an array
